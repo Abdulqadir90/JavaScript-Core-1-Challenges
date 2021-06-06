bd4f9aa45d2bb85d2384d083900293607e8d4fc2
Abdulqadir90 <33445223+Abdulqadir90@users.noreply.github.com>
2021-06-06 19:44:13 +0100

    log even nums

--- Algorithms/04-log-even-nums/log-even-nums.js
+++ Algorithms/04-log-even-nums/log-even-nums.js
@@ -1,4 +1,12 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
-var logEvenNums = function(num) {};
+var logEvenNums = function(num) {
+    let el =  Math.floor(Math.random() * num);
+    if(el % 2 === 0){
+        return el;
+    }
+};
+
+let total = logEvenNums(50);
+console.log(total)
