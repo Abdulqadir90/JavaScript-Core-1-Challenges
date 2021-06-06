ddda17df542dabedd946bafd6bf6834bc1807253
Abdulqadir90 <33445223+Abdulqadir90@users.noreply.github.com>
2021-06-06 22:11:37 +0100

    vowel count

--- Algorithms/09-vowel-count/vowel-count.js
+++ Algorithms/09-vowel-count/vowel-count.js
@@ -1,3 +1,11 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    let el = [b, e, f, g, c, d];
+    for(let i = 0; i < arr.length; i++) {
+        let num = 0;
+        if(str.toLowerCase().includes(el[i])) {
+            num++;
+        }
+    }
+};
