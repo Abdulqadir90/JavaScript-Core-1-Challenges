b2afed025c032369c8e7af345c48beacfb7dae7d
Abdulqadir90 <33445223+Abdulqadir90@users.noreply.github.com>
2021-06-06 21:55:22 +0100

    sum arrays

--- Algorithms/06-sum-array/sum-array.js
+++ Algorithms/06-sum-array/sum-array.js
@@ -1,3 +1,11 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function(arr) {
+    let total = 0;
+    for(let i = 0; i < arr.length; i++) {
+        total = total += arr[i];
+    }
+};
+
+let overallTotal = sumArray();
+console.log(overallTotal);
