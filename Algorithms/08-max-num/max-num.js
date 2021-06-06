4d06883d1a46396037334074eb56e7515460bf54
Abdulqadir90 <33445223+Abdulqadir90@users.noreply.github.com>
2021-06-06 22:08:56 +0100

    max num

--- Algorithms/08-max-num/max-num.js
+++ Algorithms/08-max-num/max-num.js
@@ -1,3 +1,11 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    var largestNum = [34, 21, 9, 10, 11];
+    for(let i = 0; i < arr.length; i++) {
+        if(largestNum < arr[i]) {
+          largestNum = arr[i]
+        }
+    } 
+}
+console.log(largestNum)
