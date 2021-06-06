cec3b65349ff651d10d5e64ba33391288e1f5900
Abdulqadir90 <33445223+Abdulqadir90@users.noreply.github.com>
2021-06-06 19:31:11 +0100

    arithmetic

--- Algorithms/01-arithmetic/arithmetic.js
+++ Algorithms/01-arithmetic/arithmetic.js
@@ -1,9 +1,25 @@
 // Fill in the body of each function with the code required
 
-var add = function(num1, num2) {};
+var add = function(num1, num2) {
+    return num1 + num2;
+};
+var result = add(10, 10);
+console.log(result);
 
-var subtract = function(num1, num2) {};
+var subtract = function(num1, num2) {
+    return num1 - num2;
+};
+var result1 = subtract(10, 7.5);
+console.log(result1);
 
-var multiply = function(num1, num2) {};
+var multiply = function(num1, num2) {
+    return num1 * num2;
+};
+var result2 = multiply(10, 100);
+console.log(result2);
 
-var divide = function(num1, num2) {};
+var divide = function(num1, num2) {
+    return num1/ num2;
+};
+var result3 = divide(1000, 50)
+console.log(result3)
